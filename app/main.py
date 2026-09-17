7f6ea0a204528223b89c0d33321643881e23ec14
Edson <eds.291120@gmail.com>
2026-09-17 12:04:29 -0300

    remove import não utilizado em main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from app.api.v1.router import api_router
-from app.core.config import settings
 from app.core.exceptions import (
     InsufficientDataError,
     MarketDataUnavailableError,
